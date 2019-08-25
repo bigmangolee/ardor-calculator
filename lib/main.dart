9017e812229411803cba258e17dd019ce02bbd2b
jinhua <ahuaness@163.com>
2019-08-25 14:41:46 +0800

    Add routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'package:anise_calculator/app/anise/calculator/cal_home.dart';
 
-void main() => runApp(CalHome());
+void main() => runApp(AniseApp());
+
+
+class AniseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Anise Calculator',
+      // MaterialApp contains our top-level Navigator
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => CalHome(),
+      },
+    );
+  }
+}
