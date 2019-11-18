b45b25bd459d79698dca461149e0f60cbc7ecce9
BigMango.Lee <bigmango@ardor.app>
2019-11-18 16:32:00 +0800

    Optimize the strength of AES encryption

--- android/app/src/main/java/app/ardor/lib/crypto/AES.java
+++ android/app/src/main/java/app/ardor/lib/crypto/AES.java
@@ -2,51 +2,49 @@
 
 package app.ardor.lib.crypto;
 
-import java.security.SecureRandom;
+import android.annotation.TargetApi;
+import android.os.Build;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import app.ardor.lib.encoding.Base58;
 
 
 public class AES {
-    public static String encrypt(String seed, String text) throws Exception {
-        byte[] rawKey = seed.getBytes();
-        byte[] result = encrypt(rawKey, text.getBytes());
-        return Hex.toHex(result);
+
+    private static byte[] ivs = "IvPara.Ardor.App".getBytes();
+
+    public static void setIvParameterSpec(String ivKey) {
+        ivs = MD5.makeMD5(ivKey).substring(0,16).getBytes();
     }
 
-    public static String decrypt(String seed, String encrypted) throws Exception {
-        byte[] rawKey = seed.getBytes();
-        byte[] enc = Hex.toByte(encrypted);
-        byte[] result = decrypt(rawKey, enc);
-        return new String(result);
-    }
-
-    private static byte[] getRawKey(byte[] seed) throws Exception {
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        sr.setSeed(seed);
-        kgen.init(128, sr); // 192 and 256 bits may not be available
-        SecretKey skey = kgen.generateKey();
-        byte[] raw = skey.getEncoded();
-        return raw;
-    }
-
-    public static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public static String encrypt(String secretKey, String encData) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        byte[] raw = secretKey.getBytes();
         SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-        byte[] encrypted = cipher.doFinal(clear);
-        return encrypted;
+
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new IvParameterSpec(ivs));
+        byte[] encrypted = cipher.doFinal(encData.getBytes("utf-8"));
+        return Base58.encode(encrypted);
     }
 
-    public static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-        byte[] decrypted = cipher.doFinal(encrypted);
-        return decrypted;
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public static String decrypt(String secretKey, String decData) {
+        try {
+            byte[] raw = secretKey.getBytes("ASCII");
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec(ivs);
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] encrypted = Base58.decode(decData);
+            byte[] original = cipher.doFinal(encrypted);
+            String originalString = new String(original, "utf-8");
+            return originalString;
+        } catch (Exception ex) {
+            return null;
+        }
     }
 }
--- android/app/src/main/kotlin/app/ardor/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/app/ardor/calculator/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import app.ardor.flutter.plugins.ArdorCryptoPlugins
 import app.ardor.flutter.plugins.FlutterPluginRegistrant
 import app.ardor.flutter.plugins.LogPlugins
+import app.ardor.lib.crypto.AES
+import app.ardor.lib.utils.AppInfoUtils
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -11,6 +13,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+      AES.setIvParameterSpec(AppInfoUtils.getSingInfo(applicationContext, packageName, AppInfoUtils.SHA1))
       GeneratedPluginRegistrant.registerWith(this)
       FlutterPluginRegistrant.addPlugins(LogPlugins::class.java)
       FlutterPluginRegistrant.addPlugins(ArdorCryptoPlugins::class.java)
